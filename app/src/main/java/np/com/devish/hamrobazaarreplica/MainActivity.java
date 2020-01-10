360f9ce5e047ac4051c4c2677b5d4f3019b0f833
Devish Baidawar Chhetri <devishbaidawar@gmail.com>
2020-01-10 13:54:47 +0545

    Models added and Product Adapter added

--- app/src/main/java/np/com/devish/hamrobazaarreplica/MainActivity.java
+++ app/src/main/java/np/com/devish/hamrobazaarreplica/MainActivity.java
@@ -2,15 +2,20 @@ package np.com.devish.hamrobazaarreplica;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import np.com.devish.hamrobazaarreplica.adapters.ImageSliderAdapter;
 
@@ -89,5 +94,109 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        //------------Category Recycler View------------//
+        rvCategory = findViewById(R.id.rvCategory);
+
+        //Create a list of category
+        List<Category> categoryList = new ArrayList<>();
+
+        categoryList.add(new Category(R.drawable.post_free_ad, "Post FREE Ad"));
+        categoryList.add(new Category(R.drawable.mobile_handset,"Mobile Handset"));
+        categoryList.add(new Category(R.drawable.laptop,"Laptops / Notebooks"));
+        categoryList.add(new Category(R.drawable.houses_for_sale,"Rental Houses"));
+        categoryList.add(new Category(R.drawable.furnitures_for_home,"Furnitures for Home"));
+        categoryList.add(new Category(R.drawable.rental_houses, "Houses for Sale"));
+        categoryList.add(new Category(R.drawable.see_more_categories, "See More Categories"));
+
+
+
+        CategoryAdapter categoryAdapter = new CategoryAdapter(categoryList,this);
+        rvCategory.setAdapter(categoryAdapter);
+        rvCategory.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        //------------Ending Category Recycler View------------//
+
+
+
+        //------------Trending Product Recycler View------------//
+        rvTrendingProducts = findViewById(R.id.rvTrendingProducts);
+
+        //Create a list of category
+        List<Products> productsList = new ArrayList<>();
+
+        productsList.add(new Products("Iphone 6s 16gb", R.drawable.iphone, "Rs.10,000", "(Used)"));
+        productsList.add(new Products("21 gear black Mountain Bike", R.drawable.mountain_bike, "Rs.10,000", "(Used)"));
+        productsList.add(new Products("Iphone 6s 16gb", R.drawable.iphone, "Rs.10,000", "(Used)"));
+        productsList.add(new Products("21 gear black Mountain Bike", R.drawable.mountain_bike, "Rs.10,000", "(Used)"));productsList.add(new Products("Iphone 6s 16gb", R.drawable.iphone, "Rs.10,000", "(Used)"));
+        productsList.add(new Products("21 gear black Mountain Bike", R.drawable.mountain_bike, "Rs.10,000", "(Used)"));
+
+
+        ProductAdapter productAdapter= new ProductAdapter(productsList,this);
+        rvTrendingProducts.setAdapter(productAdapter);
+        rvTrendingProducts.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+        //------------Ending Category Recycler View------------//
+
+
+
+
+
+        //------------Recently Listed Ads Product Recycler View------------//
+        rvRecentlyListedAds = findViewById(R.id.rvRecentlyListedAds);
+
+        //Create a list of category
+        List<Products> recentProductsList = new ArrayList<>();
+
+        recentProductsList.add(new Products("21 gear black Mountain Bike", R.drawable.mountain_bike, "Rs.10,000", "(Used)"));
+        recentProductsList.add(new Products("Iphone 6s 16gb", R.drawable.iphone, "Rs.10,000", "(Used)"));
+        recentProductsList.add(new Products("21 gear black Mountain Bike", R.drawable.mountain_bike, "Rs.10,000", "(Used)"));productsList.add(new Products("Iphone 6s 16gb", R.drawable.iphone, "Rs.10,000", "(Used)"));
+        recentProductsList.add(new Products("Iphone 6s 16gb", R.drawable.iphone, "Rs.10,000", "(Used)"));
+        recentProductsList.add(new Products("21 gear black Mountain Bike", R.drawable.mountain_bike, "Rs.10,000", "(Used)"));
+        recentProductsList.add(new Products("Iphone 6s 16gb", R.drawable.iphone, "Rs.10,000", "(Used)"));
+
+
+        ProductAdapter recentProductAdapter= new ProductAdapter(recentProductsList,this);
+        rvRecentlyListedAds.setAdapter(recentProductAdapter);
+        rvRecentlyListedAds.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+        //------------Ending Category Recycler View------------//
+    }
+
+    private boolean pagerMoved = false;
+    private static final long ANIM_VIEWPAGER_DELAY = 5000;
+
+    private Handler h = new Handler();
+
+    private Runnable animateViewPager = new Runnable() {
+        @Override
+        public void run() {
+
+            if (!pagerMoved){
+                if (viewPager.getCurrentItem() == viewPager.getChildCount()){
+                    viewPager.setCurrentItem(0,true);
+                }
+                else {
+                    viewPager.setCurrentItem(viewPager.getCurrentItem()+1, true);
+                }
+                pagerMoved = false;
+                h.postDelayed(animateViewPager, ANIM_VIEWPAGER_DELAY);
+            }
+        }
+    };
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (h != null){
+            h.removeCallbacks(animateViewPager);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        h.postDelayed(animateViewPager,ANIM_VIEWPAGER_DELAY);
     }
 }
---
+++ app/src/main/java/np/com/devish/hamrobazaarreplica/adapters/ProductAdapter.java
@@ -0,0 +1,70 @@
+package np.com.devish.hamrobazaarreplica.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.deepak.hamrobazzar.R;
+import com.deepak.hamrobazzar.model.Products;
+
+import java.util.List;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolders>{
+
+    List<Products> productsList;
+    Context context;
+
+    public ProductAdapter(List<Products> productsList, Context context) {
+        this.productsList = productsList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ProductViewHolders onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.layout_product,parent,false);
+        return new ProductViewHolders(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductViewHolders holder, int position) {
+        final Products products = productsList.get(position);
+
+        holder.tvProductName.setText(products.getProductName());
+        holder.tvPrice.setText(products.getProductPrice());
+        holder.tvUserOrNot.setText(products.getProductUseOrNot());
+
+        holder.imgProduct.setImageResource(products.getProductImage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return productsList.size();
+    }
+
+    public class ProductViewHolders extends RecyclerView.ViewHolder{
+
+        TextView tvProductName, tvPrice, tvUserOrNot;
+        ImageView imgProduct;
+
+        public ProductViewHolders(@NonNull View itemView) {
+            super(itemView);
+
+            tvProductName = itemView.findViewById(R.id.tvProductName);
+            tvPrice = itemView.findViewById(R.id.tvPrice);
+            tvUserOrNot = itemView.findViewById(R.id.tvUserOrNot);
+
+            imgProduct = itemView.findViewById(R.id.imgProduct);
+
+
+
+        }
+    }
+}
