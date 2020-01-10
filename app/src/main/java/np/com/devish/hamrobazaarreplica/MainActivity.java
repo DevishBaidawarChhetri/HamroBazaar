b5e5a333bdca31e68e09f3280f390dd479a3b8ae
Devish Baidawar Chhetri <devishbaidawar@gmail.com>
2020-01-10 13:47:30 +0545

    Image Slider Added

--- app/src/main/java/np/com/devish/hamrobazaarreplica/MainActivity.java
+++ app/src/main/java/np/com/devish/hamrobazaarreplica/MainActivity.java
@@ -1,14 +1,93 @@
 package np.com.devish.hamrobazaarreplica;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toolbar;
+
+import np.com.devish.hamrobazaarreplica.adapters.ImageSliderAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar mainToolbar;
+
+    ImageView imgLoginIcon;
+
+    //Category Recycler View
+    RecyclerView rvCategory, rvTrendingProducts, rvRecentlyListedAds;
+
+    ViewPager viewPager;
+
+    LinearLayout sliderDotsPanel;
+    private int dotscount;
+    private ImageView[] dots;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgLoginIcon = findViewById(R.id.imgLoginIcon);
+        imgLoginIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
+        viewPager = findViewById(R.id.imageSlider);
+
+        //For Indicators
+        sliderDotsPanel = (LinearLayout) findViewById(R.id.sliderDots);
+
+
+        ImageSliderAdapter adapter = new ImageSliderAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        //Indicator
+        dotscount = adapter.getCount();
+        dots = new ImageView[dotscount];
+
+        for (int i = 0; i < dotscount; i++) {
+
+            dots[i] = new ImageView(this);
+            dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.default_indicator    ));
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            params.setMargins(8, 0, 8, 0);
+
+            sliderDotsPanel.addView(dots[i], params);
+
+        }
+
+        dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.selected_indicator));
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                for (int i = 0; i < dotscount; i++) {
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.default_indicator));
+                }
+                dots[position].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.selected_indicator));
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 }
